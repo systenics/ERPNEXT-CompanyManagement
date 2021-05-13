a1482188532cab2d1d54df203b07b3cc22842d2d
Saurabh Nandu <saurabh@systenics.com>
2021-05-13 18:55:24 +0530

    Appraisal Grading

---
+++ company_management/company_management/doctype/appraisal_grading_scale/appraisal_grading_scale.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Systenics and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Appraisal Grading Scale', {
+	// refresh: function(frm) {
+
+	// }
+});
